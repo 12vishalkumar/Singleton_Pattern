c5dd02447afe58e61002ebabd75355e3435fe2c7
12vishalkumar <90981381+12vishalkumar@users.noreply.github.com>
2022-04-15 00:18:16 +0530

    Add files via upload

---
+++ ptc.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class ptc
+{
+
+    public static void main(String[] args) 
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String st1 = sc.next();
+        String st2 = sc.next();
+        System.out.println("Hello I am a singleton! Let me say "+st1+" "+st2+" to you");
+        
+    }
+}
